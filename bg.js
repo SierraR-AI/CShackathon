0c51f38e38e3dd63649b73dfcba2ebf225470a7b
Sierra Alexis <sierraalexis@Sierras-Air.lan>
2024-10-29 15:25:24 -0400

    JJ

--- bg.js
+++ bg.js
@@ -1,10 +1,76 @@
-// fetch("https://api.soundcloud.com")
-//   .then((res) => res.json())
-//   .then((res) => {
-//     return res;
-//   });
+// chrome.runtime.onInstalled.addListener(function () {
+//   var audio = new Audio"https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1");
 
-// fetch("https://api.soundcloud.com", {
-//   method: "GET",
-//   body: new FormData(),
+//   audio.play();
 // });
+
+
+// Allows cross browser functionality
+const bgAPI = (typeof browser !== 'undefined') ? browser : chrome;
+ 
+let videoId = null;
+let reminder; 
+
+function waterNotification(){
+  bgAPI.notifications.create({
+    type:"basic",
+    iconUrl: "moodz.png",
+    title: "Water Reminder",
+    message: "It's time to drinkk water!",
+    priority: 2
+  });
+}
+function startWaterReminder(){
+  clearInterval(reminderInterval);
+    reminderInterval = setInterval(sendWaterNotification, 3600000);
+}
+function stopWaterReminder() {
+  clearInterval(reminderInterval);
+}
+
+
+bgAPI.runtime.onInstalled.addListener(function () {
+  openVideo(); // opens when extension is installed 
+});
+
+function openVideo(){
+  if (videoId !== null){ 
+    bgAPI.tabs.update(videoId, { active: true});
+  }else {
+    bgAPI.tabs.create({ url: "https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1"}, (tab) => {
+      videoId = tab.id; 
+        });
+    }
+  }
+bgAPI.runtime.onSuspend.addListener(function () {
+  if (videoId !== null) {
+      bgAPI.tabs.remove(videoId);
+      videoId = null;
+  }
+});
+ 
+bgAPI.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "startReminder") {
+      startWaterReminder();
+      sendResponse({ status: "Water reminders started." });
+  } else if (request.action === "stopReminder") {
+      stopWaterReminder();
+      sendResponse({ status: "Water reminder has stopped." });
+  }
+});
+
+bgAPI.runtime.onSuspend.addListener(function () {
+  if (videoId !== null) {
+      bgAPI.tabs.remove(videoId);
+      videoId = null;
+  }
+});
+
+
+// let timerInterval;
+// let totalSeconds = 24 * 60; 
+// let isTimerRunning = false;
+ 
+// function startTimer() {
+//   if (totalSeconds =)
+// }
--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
-// ope\
+pipButton.addEventListener("click", async () => {
+    const player = document.querySelector("#player");
+  
+    if (!document.pictureInPictureEnabled) {
+      console.error("Picture-in-Picture is not supported by this browser.");
+      return;
+    }
+  
+    if (!player) {
+      console.error("Player element not found.");
+      return;
+    }
+  
+    try {
+      if (document.pictureInPictureElement) {
+        // If already in Picture-in-Picture, exit it
+        await document.exitPictureInPicture();
+      } else {
+        // Request Picture-in-Picture on the video element
+        await player.requestPictureInPicture();
+      }
+    } catch (error) {
+      console.error("Error with Picture-in-Picture:", error);
+    }
+  });
+  
